76ff84cef069a07c21afe19406dc1eafae471765
Ahmad Yazid Munif <ahmadyazid76@gmail.com>
2024-12-05 09:41:07 +0700

    configure npx install shadcn and hook-form

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
